396430688a852787e41f47279de598006047c72b
ZhanyiTu <125548913@qq.com>
2019-12-03 05:06:22 -0800

    学习了一下管道的使用，开始写代码把

---
+++ lab1/lab1/learn3.c
@@ -0,0 +1,10 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<signal.h>
+//int main(void){
+//int pipefd[2];
+//int pipe(pipefd); //创建无名管道 pipe[0]只能读 pipe[1]只能写
+//write(pipefd[0], )
+//
+//
+//}
